fc32671b5a2dca1fd183dbb06dc743d713a282f2
Ryosuke Kawasaki <ruuk0220@gmail.com>
2022-09-16 01:35:41 +0000

    commit

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,10 +1,29 @@
 import type { NextPage } from "next";
 import { ButtonSample } from "../components/Button";
+import styled from "@emotion/styled";
+import Grid from "@mui/material/Grid";
+import { ButtonAppBar } from "../components/AppBar";
 
 const Home: NextPage = () => {
   return (
     <>
-      <ButtonSample />
+      {/* <ButtonSample /> */}
+      <ButtonAppBar />
+
+      <Grid container spacing={2}>
+        <Grid item xs={6} md={8}>
+          <ButtonSample />
+        </Grid>
+        <Grid item xs={6} md={4}>
+          test2
+        </Grid>
+        <Grid item xs={6} md={4}>
+          test3
+        </Grid>
+        <Grid item xs={6} md={8}>
+          test4
+        </Grid>
+      </Grid>
     </>
   );
 };
